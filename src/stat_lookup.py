2ce9c75ab2fa8c1c558cd0e41cee2c205fd93991
Liam <liam.yy.xu@gmail.com>
2023-02-06 15:10:18 -0500

    Continued stat lookup

--- src/stat_lookup.py
+++ src/stat_lookup.py
@@ -9,13 +9,15 @@ with open("assets/custom_roster/students/data.json", 'r') as f:
 
 sorted_datasets = []
 
-# TODO: fix
-for p, value in data.items():
-    ranked = sorted(value, key=lambda d: d[stat_name])
+for stat_name in [
+    "total_kills", "successful_defenses", "attempted_attacks", "attempted_defenses",
+    "attempted_interventions", "kills_off_attacks", "kills_off_defenses", "kills_off_interventions",
+    "rounds_survived", "wins", "total_games"
+]:
+    ranked = sorted(data, key=lambda d: d["stats"][stat_name])
     sorted_datasets.append(ranked)
 
 print(sorted_datasets)
 
 for stat_name, value in player.stats.items():
     print(f"{stat_name}: {value}")
-
